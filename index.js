a794f7c668ef26612e7370b998443cfdf5888983
Francesco Fiacco <fiacco.fra@gmail.com>
2018-07-27 11:06:46 +0100

    moved scraping logic in promise

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 program
   .version('0.0.1')
   .option('-p, --posts <required>','number of top posts to print')
   .parse(process.argv);
 
-const rp = require('request-promise');
-const cheerio = require('cheerio');
 const pages = Math.ceil(program.posts/30);
 
-for (let i = 1; i <= pages; i++){
-    console.log("here");
-    let options = {
-        uri: `https://news.ycombinator.com/news?p=` + i,
-        transform: function (body) {
-            return cheerio.load(body);
-        }
-    };
-
-    console.log(options.uri);
-
-    rp(options)
-    .then(($) => {
-        $('td.title .storylink').map(function(num, el) {
-            if (((i-1) * 30) + num < program.posts)
-                console.log((i-1) * 30 + num + ": " + $(this).text());
+let scrape = new Promise((resolve, reject) => {
+    let result = {
+        articles: []
+    }
+    for (let i = 1; i <= pages; i++){
+        let options = {
+            uri: `https://news.ycombinator.com/news?p=` + i,
+            transform: function (body) {
+                return cheerio.load(body);
+            }
+        };
+    
+        rp(options)
+        .then(($) => {
+            $('td.title .storylink').map(function(num, el) {
+                if (((i-1) * 30) + num < program.posts){
+                    result.articles.push({
+                        title: $(this).text()
+                    });
+                }
+                if (((i-1) * 30) + num < program.posts){
+                    resolve(result);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            reject("Couldn't scrape!");
         });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-}
+    }
+});
+
+scrape.then((result) => {
+    console.log(JSON.stringify(result.articles));
+});
