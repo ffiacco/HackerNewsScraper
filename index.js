b51f4e330ae18bbaf30f8b96decd89fbd19905ff
Francesco Fiacco <fiacco.fra@gmail.com>
2018-07-27 13:10:41 +0100

    input check

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cheerio = require('cheerio');
 
 program
   .version('0.0.1')
-  .option('-p, --posts <required>','number of top posts to print')
+  .option('-p, --posts <required>','number of top posts to print', parseInt)
   .parse(process.argv);
 
 const pages = Math.ceil(program.posts/30);
@@ -45,11 +45,14 @@ let scrape = () => {
                         result.articles[((i-1) * 30) + num].points = $(this).text().split(" ")[0];
                     }
                 });
-                
                 $('td.subtext :nth-child(6)').map(function(num) {
                     if (((i-1) * 30) + num + 1 <= program.posts){
                         result.articles[((i-1) * 30) + num].comments = $(this).text().split("&")[0];
-                        result.articles[((i-1) * 30) + num].rank = (i-1) * 30 + num + 1;
+                    }
+                });
+                $('span.rank').map(function(num) {
+                    if (((i-1) * 30) + num + 1 <= program.posts){
+                        result.articles[((i-1) * 30) + num].rank = $(this).text().split(".")[0];
                     }
                 });
                 resolve("Success!");
@@ -64,8 +67,13 @@ let scrape = () => {
     }
 };
 
-scrape();
+if (0 <= program.posts && program.posts <= 100){
+    scrape();
+    Promise.all(promiseChain).then(() => {
+        console.log(JSON.stringify(result.articles, null, '\t'));
+    });
+}   
+else 
+    console.log("Invalid Input");
 
-Promise.all(promiseChain).then(() => {
-    console.log(JSON.stringify(result.articles, null, '\t'));
-})
+
